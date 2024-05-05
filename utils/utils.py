12bd5ad434c946488044f1c8b860433771b34d76
adriansanj <adriansanjuanes@gmail.com>
2024-05-05 20:10:11 +0200

    Added general and specific metrics and changed how to visualize them

--- utils/utils.py
+++ utils/utils.py
@@ -2,13 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import threading
+import tensorflow as tf
 
 
 def load_parameter_list():
     #Loading list of parameters
     parameters = []
 
-    for i in range(5):
+    for i in range(10):
         file_params = f'./datos_tfg/datos_tfg/tfg_parametros_{i+1}.txt'
         with open(file_params, 'r') as file:
             for line in file:
@@ -19,7 +20,7 @@ def load_parameter_list():
                 
     return parameters
 
-def visualize(idx_list, parameters, cols = 2):
+def visualize(idx_list, parameters, cols = 2, file = None):
     
     file_paths = [f'./datos_tfg/datos_tfg/tfg_datos_{idx[0]}_{idx[1]}.txt' for idx in idx_list]
     
@@ -44,9 +45,13 @@ def visualize(idx_list, parameters, cols = 2):
         
     
     plt.tight_layout()
+    if file:
+        plt.savefig(file)
     plt.show()
 
-def visualize_parameter_distributions(df_params, y_limit):
+    
+
+def visualize_parameter_distributions(df_params, y_limit, file = None):
   fig, axes = plt.subplots(2, 4, figsize=(12, 8))
 
   axes = axes.flatten()
@@ -63,7 +68,11 @@ def visualize_parameter_distributions(df_params, y_limit):
 
   axes[-1].axis('off')
   plt.tight_layout()
+  if file:
+        plt.savefig(file)
   plt.show()
+
+  
 
 def save_indexes(idx_list, file_path):
     with open(file_path, 'w') as file:
@@ -128,17 +137,27 @@ def load_data_multithreaded(idx_list, threads_n, small=False, debug=False):
     cleaned_results  = [tupl[1] for tupl in ordered_results]
     return cleaned_results
 
+def r_squared(y_true_slice, y_pred_slice):
+    numerator = tf.reduce_sum(tf.square(tf.subtract(y_true_slice, tf.reduce_mean(y_true_slice))))
+    denominator = tf.reduce_sum(tf.square(tf.subtract(y_true_slice, y_pred_slice)))
+    return 1 - tf.math.divide_no_nan(numerator, denominator + tf.keras.backend.epsilon())
 
-def plot_metrics(history, metric_name):
+def rmse(y_true_slice, y_pred_slice):
+    mse = tf.keras.losses.mean_squared_error(y_true_slice, y_pred_slice) 
+    return tf.sqrt(mse + tf.keras.backend.epsilon())
+
+def plot_metrics(history, metric_name,lim_y_bottom = 0, lim_y_top = 1.6, file = None):
     plt.plot(history.history[metric_name], color='navy')
     plt.plot(history.history[f'val_{metric_name}'], color='red')
-    plt.title(metric_name.capitalize())
+    plt.title(f'Train and validation {metric_name} during training')
     plt.ylabel(metric_name.upper())
     plt.xlabel('Epoch')
     plt.legend(['Train', 'Validation'], loc='upper right')
-    plt.grid(True)  # Add grid
-    plt.ylim(bottom=0)  # Set lower limit of y-axis to 0
-    plt.ylim(top=1.5)  # Set upper limit of y-axis to 1
+    plt.grid(True)  
+    plt.xlim(left=0)
+    plt.ylim(bottom=lim_y_bottom)  
+    plt.ylim(top=lim_y_top)  
+    plt.savefig(file)
     plt.show()
 
 def model_evaluation(model, X_test, y_test, random_samples = 5):
@@ -146,13 +165,40 @@ def model_evaluation(model, X_test, y_test, random_samples = 5):
     print('Test mse:', metrics[0])
     print('Test rmse:', metrics[1])
     print('Test mae:', metrics[2])
-    print('Test r2score:', metrics[3])
+    print('Test R2:', metrics[3])
+
+
+    # Separate ground truth and predictions for the last two outputs
+    y_true_e1 = y_test['e1']
+    y_true_e2 = y_test['e2']
+    predictions = model.predict(X_test)
+    predictions_e1 = predictions[:, -2]
+    predictions_e2 = predictions[:, -1]
+
+    # Calculate metrics independently for each output
+    #e1
+    mse_e1 = tf.keras.metrics.mean_squared_error(y_true_e1, predictions_e1)
+    rmse_e1 = rmse(y_true_e1, predictions_e1)
+    mae_e1 = tf.keras.metrics.mean_squared_error(y_true_e1, predictions_e1)
+    r2_e1 = r_squared(y_true_e1, predictions_e1)
+
+    mse_e2 = tf.keras.metrics.mean_squared_error(y_true_e2, predictions_e2)
+    rmse_e2 = rmse(y_true_e2, predictions_e2)
+    mae_e2 = tf.keras.metrics.mean_squared_error(y_true_e1, predictions_e2)
+    r2_e2 = r_squared(y_true_e2, predictions_e2)
+
+    print("Metrics for parameters e1 and e2:")
+    print(f"Parameter e1:\n  \tMSE: {mse_e1:.3f}, RMSE: {rmse_e1:.3f}, MAE: {mae_e1:.3f}, R2: {r2_e1:.3f}")
+    print(f"Parameter e2:\n  \tMSE: {mse_e2:.3f}, RMSE: {rmse_e2:.3f}, MAE: {mae_e2:.3f}, R2: {r2_e2:.3f}")
+
+
     random_indices = np.random.choice(len(y_test), size=random_samples, replace=False)
     test_samples = X_test[random_indices]
     predictions = model.predict(test_samples)
-    print('\n\nRandom prediction examples')
+    print('\nRandom prediction examples')
     print('Parameters:\tcx\tcy\ta\tb\ttheta\te1\te2')
     for i in range(len(predictions)):
         print('------------')
         print('real:\t\t', '\t'.join(f'{val:.3f}' for val in y_test.iloc[random_indices[i]]))
         print('prediction:\t', '\t'.join(f'{val:.3f}' for val in predictions[i]))
+
